fc4e892e21c34034c9b68952ddf8187b84a26a44
tech241 <suhaniarora717@gmail.com>
2025-11-28 12:00:20 +0800

    added BookingItem class to store bookings under main > java > models

---
+++ CICD Assignment 1-2/src/main/java/models/BookingItem.java
@@ -0,0 +1,15 @@
+package models;
+
+public class BookingItem {
+    public String packageId;
+    public String serviceId;
+    public String packageName;
+    public String serviceName;
+    public String date;
+    public String time;
+    public String notes;
+    public String caretaker;
+    public String price;
+
+    public BookingItem() {} // default constructor
+}
