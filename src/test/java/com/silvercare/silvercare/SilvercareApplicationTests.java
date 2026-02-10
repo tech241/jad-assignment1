09c4c8ea33d02106588265999c034596cdd08569
Phoenix226-a <cedricmok.24@ichat.sp.edu.sg>
2026-02-10 16:44:42 +0800

    Created Spring Boot

---
+++ src/test/java/com/silvercare/silvercare/SilvercareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvercare.silvercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvercareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
